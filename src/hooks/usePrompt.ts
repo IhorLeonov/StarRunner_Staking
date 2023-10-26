febaf3846fe6aae788270b0c5402a2338e1cbe8a
Ihor Leonov <ihorleonow@gmail.com>
2023-10-26 21:03:25 +0300

    Update usePrompt.ts

--- src/hooks/usePrompt.ts
+++ src/hooks/usePrompt.ts
@@ -1,27 +1,29 @@
 import { useState } from "react";
 // import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
-const body = document.querySelector("body") as HTMLBodyElement;
+// const body = document.querySelector("body") as HTMLBodyElement;
 
 export const usePrompt = () => {
   const [promptName, setPromptName] = useState<string | null>(null);
   const [promptClass, setPromptClass] = useState<string | null>(null);
-  const [scroll, setScroll] = useState<boolean>(true);
+  // const [scroll, setScroll] = useState<boolean>(true);
 
-  body.style.overflowY = scroll ? "scroll" : "hidden";
+  // body.style.overflow = !scroll ? "scroll" : "hidden";
 
   const handleShowPrompt = (name: string) => {
     setPromptName(name);
     setPromptClass(`${name}_prompt`);
     // disableBodyScroll(body);
-    setScroll(false);
+    // setScroll(false);
+    document.body.style.overflow = "hidden";
   };
 
   const handleHidePrompt = () => {
     setPromptName(null);
     setPromptClass(null);
     // enableBodyScroll(body);
-    setScroll(true);
+    // setScroll(true);
+    document.body.style.overflow = "scroll";
   };
 
   return {
